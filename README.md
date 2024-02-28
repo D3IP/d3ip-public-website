1eebd428b956c6c103e03c23e9ca9cfb3a7f1c11
Will D <willd@d3ip.io>
2024-02-28 11:45:19 +0000

    Initial config/testing for workflow in new AWS Account

---
+++ README.md
@@ -0,0 +1,3 @@
+# D3IP Public Website
+Content exported from wordpress using Simply Static plugin
+GitHub Actions deploy code to stage site on push and to production on manual job run
---
+++ scripts/upload.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set script to echo all the output when running
+set -x
+
+# Check if the file name was provided
+if [ -z "$1" ]
+then
+    echo "No file provided"
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f "$1" ]
+then
+    echo "File does not exist"
+    exit 1
+fi
+
+# Copy the file to the destination directory
+cp "$1" ./static-site-zip/
+
+# Change to the destination directory
+cd ./static-site-zip/ || exit
+
+# Copy the file the name used by pipeline job
+cp "$1" "simply-static.zip"
+
+# Add the file to the git repo
+git add .
+
+# Commit the changes
+git commit -m "Added file $1"
+
+# Push to repo
+git push
